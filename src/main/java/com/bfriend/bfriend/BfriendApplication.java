b2bdd90f481f8932b59d0ea63f889fec72c680c2
haneul0509 <haneul0509@naver.com>
2024-11-24 23:23:18 +0900

    init new project

---
+++ src/main/java/com/bfriend/bfriend/BfriendApplication.java
@@ -0,0 +1,13 @@
+package com.bfriend.bfriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BfriendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BfriendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bfriend/bfriend/BfriendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bfriend.bfriend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BfriendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
