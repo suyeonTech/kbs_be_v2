c8775bfa02bd584b7280ef39aa633495c40b5d16
chae02yo <chae02kim@naver.com>
2024-12-18 15:14:42 +0900

    refactor: 이메일 발신인 값 받아오기 수정
    application.properties에서 값을 받아오는 방법으로 수정

--- src/main/java/com/bfriend/bfriend/users/MailService.java
+++ src/main/java/com/bfriend/bfriend/users/MailService.java
@@ -4,6 +4,7 @@ import com.bfriend.bfriend.users.dto.request.CheckAuthenticationNumberRequest;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -14,12 +15,15 @@ public class MailService {
     private final JavaMailSender javaMailSender;
     private final MailAuthenticationNumberService mailAuthenticationNumberService;
 
+    @Value("${spring.mail.username}")
+    private String emailSender;
+
     public MimeMessage createMail(String email, String authenticationNumber) {
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
         try {
             MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
             mimeMessageHelper.setTo(email);
-            mimeMessageHelper.setFrom("chae02yo@gmail.com");
+            mimeMessageHelper.setFrom(emailSender);
             mimeMessageHelper.setSubject("[밥친구] 이메일 인증 번호 발송");
             mimeMessageHelper.setText(authenticationNumber);
         } catch (MessagingException e) {
