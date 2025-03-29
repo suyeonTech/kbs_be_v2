e82209d09de4594487842f1eb5f925a502dd0cae
chae02yo <chae02kim@naver.com>
2025-03-29 15:31:26 +0900

    chore: 토큰 만료시간 수정

--- src/main/java/com/bfriend/bfriend/users/service/JoinService.java
+++ src/main/java/com/bfriend/bfriend/users/service/JoinService.java
@@ -32,7 +32,7 @@ public class JoinService {
         Users users = buildUsers(joinRequest);
         userRepository.save(users);
 
-        String token = jwtUtil.createJwt(users.getEmail(), users.getRole(), JWTConstants.TOKEN_VALIDITY_MILLISECONDS_1HOUR * 24); // 24시간 유효한 토큰
+        String token = jwtUtil.createJwt(users.getEmail(), users.getRole(), JWTConstants.TOKEN_VALIDITY_MILLISECONDS_1HOUR);
 
         record SignupResponse(int status, String message, String nickname) {}
         return ResponseEntity
