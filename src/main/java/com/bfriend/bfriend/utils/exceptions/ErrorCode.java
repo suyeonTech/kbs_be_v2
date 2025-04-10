6b12a558ad290e2fb0d1835ddc54a1d7c995da7d
dadamji34 <theka0304@gmail.com>
2025-04-10 17:16:48 +0900

    fix: 예외처리 결과 401만 뜨는 문제 수정

--- src/main/java/com/bfriend/bfriend/utils/exceptions/ErrorCode.java
+++ src/main/java/com/bfriend/bfriend/utils/exceptions/ErrorCode.java
@@ -13,21 +13,22 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     // AUTH
-    DUPLICATE_EMAIL(BAD_REQUEST, "AUTH_001", "이미 사용 중인 이메일입니다."),
-    INVALID_INPUT_VALUE(BAD_REQUEST, "AUTH_002", "입력값이 유효하지 않습니다."),
-    INVALID_CREDENTIALS(UNAUTHORIZED, "AUTH_003", "이메일 또는 비밀번호가 잘못되었습니다."),
-    AUTHENTICATION_FAILED(UNAUTHORIZED, "AUTH_004", "인증에 실패하였습니다."),
-    AUTHENTICATION_HEADER_MISSING(UNAUTHORIZED, "AUTH_005", "Authorization 헤더가 없습니다."),
-    TOKEN_EXPIRED(UNAUTHORIZED, "AUTH_006", "토큰이 만료되었습니다."),
-    LOGOUT_TOKEN_USED(UNAUTHORIZED, "AUTH_007", "로그아웃된 토큰입니다."),
+    AUTHENTICATION_FAILED(UNAUTHORIZED, "AUTH_001", "인증에 실패하였습니다."),
+    AUTHENTICATION_HEADER_MISSING(UNAUTHORIZED, "AUTH_002", "Authorization 헤더가 없습니다."),
+    TOKEN_EXPIRED(UNAUTHORIZED, "AUTH_003", "토큰이 만료되었습니다."),
+    LOGOUT_TOKEN_USED(UNAUTHORIZED, "AUTH_004", "로그아웃된 토큰입니다."),
 
     //ROOM
     ROOM_NOTFOUND(BAD_REQUEST, "ROOM_001", "존재하지 않는 모임방입니다."),
 
     // USERS
-    USERS_EMAILNOTFOUND(BAD_REQUEST, "USERS_001", "존재하지 않는 이메일입니다."),
-    USERS_AUTHENTICIATIONNUMBERNOTFOUND(BAD_REQUEST, "USERS_002", "이메일 인증 번호가 맞지 않습니다."),
-    USERS_UIDNOTFOUND(BAD_REQUEST, "USERS_003", "존재하지 않는 사용자 ID입니다.");
+    DUPLICATE_EMAIL(BAD_REQUEST, "USERS_001", "이미 사용 중인 이메일입니다."),
+    INVALID_INPUT_VALUE(BAD_REQUEST, "USERS_002", "입력값이 유효하지 않습니다."),
+    INVALID_CREDENTIALS(UNAUTHORIZED, "USERS_003", "이메일 또는 비밀번호가 잘못되었습니다."),
+
+    USERS_EMAILNOTFOUND(BAD_REQUEST, "USERS_004", "존재하지 않는 이메일입니다."),
+    USERS_AUTHENTICIATIONNUMBERNOTFOUND(BAD_REQUEST, "USERS_005", "이메일 인증 번호가 맞지 않습니다."),
+    USERS_UIDNOTFOUND(BAD_REQUEST, "USERS_006", "존재하지 않는 사용자 ID입니다."),;
 
     private final HttpStatus status;
     private final String code;
